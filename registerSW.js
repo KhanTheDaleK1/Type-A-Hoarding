1c3f5d4511fa932dc3aa6000f8a49461bdcf1463
KhanTheDaleK1 <evan.t.beechem@gmail.com>
2026-06-18 21:49:42 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Type-A-Hoarding/sw.js', { scope: '/Type-A-Hoarding/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
